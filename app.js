1059601465aced86f0c1eaf659fbfe46cd75864a
Tech1997 <dev.honing@gmail.com>
2023-10-19 09:57:50 +0900

    모듈 import

---
+++ app.js
@@ -0,0 +1,6 @@
+// 내장 모듈 import
+const http = require("http");
+const fs = require("fs");
+// 외장 모듈 import
+const data = require("../develop-training-11-signup/basic-data/basic-sign-up-asset");
+console.log(data) // 확인
