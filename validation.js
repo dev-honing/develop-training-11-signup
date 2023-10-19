65f3573d48a74b52a3d7b9f985557306e9afc0b8
Tech1997 <dev.honing@gmail.com>
2023-10-19 14:13:43 +0900

    ID 입력란에 a를 타이핑했을 때, 배경색이 변화하는지 확인 - 실패

--- validation.js
+++ validation.js
@@ -3,3 +3,10 @@
 // * 2. PW 두 개의 입력값이 같아야 함
 // * 3. Email 입력은 @이 있어야 하고, @ 뒤에는 .이 존재해야 함
 // * 4. 모든 사항을 충족했을 때에만 회원가입 버튼이 동작하도록 함
+
+const ID = document.getElementById("ID");
+const ID_content = document.getElementById("ID_content");
+const root = document.getElementById("root");
+if (ID_content.innerText  === a) {
+  root.style.backgroundColor = "red";
+}
