42f544a11ad2f997f2f0d4aec33d38cec01483d5
Tech1997 <dev.honing@gmail.com>
2023-10-19 10:08:37 +0900

    createServer() 메서드로 서버 만들기 + 초기 진입 시 이동할 index.html 생성

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ const http = require("http");
 const fs = require("fs");
 // 외장 모듈 import
 const data = require("../develop-training-11-signup/basic-data/basic-sign-up-asset");
-console.log(data) // 확인
+
+// 1. 서버 만들기 - createServer() 메서드
+http.createServer((req, res) => {
+  // 최초 진입: index.html 열기
+  if (req.method === "GET" && req.url === "/") {
+    fs.readFile("./index.html; charset=utf-8")
+  }
+})
