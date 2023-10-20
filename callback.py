a30a3c1f9e1db66ca1ad241f0d54235f952ffe15
Tech1997 <dev.honing@gmail.com>
2023-10-20 15:24:53 +0900

    매개변수의 숫자별 파이썬 콜백함수 정의와 호출

--- callback.py
+++ callback.py
@@ -1,6 +1,22 @@
-# 파이썬에서도 콜백함수가 가능한가?
+# 콜백함수
+
+# * 매개변수가 하나
+# 정의
+def call(func):
+  func()
+
+def hello():
+  print("hello")
+
+# 호출
+call(hello)
+
+# todo 매개변수가 하나일 때, 두 개의 함수를 정의하지 않고, 하나로 묶어 정의하는 방식이 있는지 알아보기
+
+# * 매개변수가 둘 
+# 정의
 def add(a,b):
   return a+b
 
+# 호출
 print(add(2,3))
-# 가능한 모습!
