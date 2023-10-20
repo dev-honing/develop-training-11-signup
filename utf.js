27f5158eb613703410a968bb2668010952c9d5a2
Tech1997 <dev.honing@gmail.com>
2023-10-20 11:42:32 +0900

    반대로 디코딩도 진행해보기

--- utf.js
+++ utf.js
@@ -1,6 +1,11 @@
 // UTF-8로 변환 연습
 
-// TextEncoder 사용해보기
+// TextEncoder() 사용해보기
 const encoder = new TextEncoder();
 const UTF_8_Arr = encoder.encode("안녕");
-console.log(UTF_8_Arr); // [236, 149, 136, 235, 133, 149]
+console.log(UTF_8_Arr); // Uint8Array(6) [236, 149, 136, 235, 133, 149]
+
+// TextDecoder()로 다시 변환
+const decoder = new TextDecoder();
+const toSTR = decoder.decode(UTF_8_Arr);
+console.log(toSTR);
