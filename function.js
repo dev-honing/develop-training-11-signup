12e3e7d7a14ebc569e0485c508cedc6948c78496
Tech1997 <dev.honing@gmail.com>
2023-10-20 14:36:14 +0900

    익명함수 정의와 호출

--- function.js
+++ function.js
@@ -8,3 +8,13 @@ function outerOne(){
 // * 호출
 console.log(outerOne); // [Function: outerOne]
 console.log(outerOne()); // hello
+
+// 2. 익명함수
+// * 정의
+const outerTwo = function(){
+  return "bye";
+}
+// * 호출
+console.log(outerTwo); // [Function: outerTwo]
+console.log(outerTwo()); // bye
+// ! 변수로 선언했기 때문에 변수 자체를 호출하면 될 것 같지만 사실상 함수 이름처럼 호출해야 함
